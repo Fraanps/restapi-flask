37b11cfee737e6ea3ce393159e591b94eb9288da
Francilene Silva <psilva.francilene@gmail.com>
2024-11-10 21:29:11 +0000

    fix: adding 0.0.0.0 due to connection

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ class Users(Resource):
 
 
 class User(Resource):
+
     def post(self):
         return {"message": "teste"}
 
@@ -22,4 +23,4 @@ api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
