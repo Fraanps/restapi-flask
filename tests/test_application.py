39620e9c0b50f85905e92d72c0e7c18d18e269b4
Francilene Silva <psilva.francilene@gmail.com>
2025-04-08 21:41:11 +0100

    test: test post user

--- tests/test_application.py
+++ tests/test_application.py
@@ -9,6 +9,35 @@ class TestApplication:
         app = create_app(MockConfig, use_mock=True)
         return app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "name": "Francilene",
+            "last_name": "Silva",
+            "cpf": "286.324.800-65",
+            "email": "fran@gmail.com",
+            "birth_date": "1994-04-08"
+        }
+
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "name": "Francilene",
+            "last_name": "Silva",
+            "cpf": "018.396.920-16",
+            "email": "fran@gmail.com",
+            "birth_date": "1994-04-08"
+        }
+
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"CPF is invalid" in response.data
